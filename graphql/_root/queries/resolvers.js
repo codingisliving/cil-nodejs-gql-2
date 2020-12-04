ebb475272ba9e047d64f88d7deb86654d7b2d9be
Ericko Yap <ericko.yaputro@btpn.com>
2020-12-04 20:21:07 +0700

    Add Users and User query

--- graphql/_root/queries/resolvers.js
+++ graphql/_root/queries/resolvers.js
@@ -4,7 +4,9 @@ const baseUrl = 'http://localhost:4001';
 const Query = {
     helloworld: () => 'helloworld',
     banks: async () => await get(`${baseUrl}/banks`) || [],
-    bank: async (_, params) => await get(`${baseUrl}/banks/${params.id}`) || {}
+    bank: async (_, params) => await get(`${baseUrl}/banks/${params.id}`) || {},
+    users: async () => await get(`${baseUrl}/users`) || [],
+    user: async (_, params) => await get(`${baseUrl}/users/${params.id}`) || {}
 };
 
 
--- graphql/index.js
+++ graphql/index.js
@@ -2,17 +2,20 @@ const { gql } = require('apollo-server-express');
 const root = require('./_root');
 const helloworld = require('./helloworld');
 const banks = require('./banks');
+const users = require('./users');
 
 const typeDefs = gql`${[
     root.schemas,
     helloworld.schemas,
-    banks.schemas
+    banks.schemas,
+    users.schemas
 ].reduce((acc, schemas) => { return acc + schemas })}`;
 
 const resolvers = {
     ...root.resolvers,
     ...helloworld.resolvers,
-    ...banks.resolvers
+    ...banks.resolvers,
+    ...users.resolvers
 };
 
 module.exports = {
