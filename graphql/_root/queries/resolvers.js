609fcbd50c1f139c0424567074ede912b66aad47
Ericko Yap <ericko.yaputro@btpn.com>
2020-12-05 11:59:21 +0700

    Add transactions and transaction query

--- graphql/_root/queries/resolvers.js
+++ graphql/_root/queries/resolvers.js
@@ -6,7 +6,9 @@ const Query = {
     banks: async () => await get(`${baseUrl}/banks`) || [],
     bank: async (_, params) => await get(`${baseUrl}/banks/${params.id}`) || {},
     users: async () => await get(`${baseUrl}/users`) || [],
-    user: async (_, params) => await get(`${baseUrl}/users/${params.id}`) || {}
+    user: async (_, params) => await get(`${baseUrl}/users/${params.id}`) || {},
+    transactions: async () => await get(`${baseUrl}/transactions`) || [],
+    transaction: async (_, params) => await get(`${baseUrl}/transactions/${params.id}`) || {},
 };
 
 
--- graphql/index.js
+++ graphql/index.js
@@ -3,19 +3,22 @@ const root = require('./_root');
 const helloworld = require('./helloworld');
 const banks = require('./banks');
 const users = require('./users');
+const transactions = require('./transactions');
 
 const typeDefs = gql`${[
     root.schemas,
     helloworld.schemas,
     banks.schemas,
-    users.schemas
+    users.schemas,
+    transactions.schemas
 ].reduce((acc, schemas) => { return acc + schemas })}`;
 
 const resolvers = {
     ...root.resolvers,
     ...helloworld.resolvers,
     ...banks.resolvers,
-    ...users.resolvers
+    ...users.resolvers,
+    ...transactions.resolvers
 };
 
 module.exports = {
